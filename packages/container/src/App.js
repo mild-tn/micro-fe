66e95d57f97ae2ba490d0ddcf82db41df677bd8c
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-08-27 23:09:26 +0700

    Update font

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hey There !!</h1>
+      <h1 style={{ color: "#5BBBF7" }}>Hey There !</h1>
       <hr />
       <MarketingApp />
     </div>
