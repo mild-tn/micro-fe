bf8f6eaf815ab6341426b3885d7ce7f572f94e10
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2022-01-26 23:42:03 +0700

    🌻 Add new service

---
+++ react-w-next/app2/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import Button from "./Button/Button";
+
+const App = () => (
+  <div>
+    <h1 className="text-xl">Typescript</h1>
+    <h2>App 2</h2>
+    <Button>Hello Test</Button>
+  </div>
+);
+
+export default App;
